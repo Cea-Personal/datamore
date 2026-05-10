8f468029a705ec7bf6f6a2617c260316b83bfbd4
basilcea <19818425+basilcea@users.noreply.github.com>
2026-05-11 00:05:09 +0200

    update frontpage

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,14 +1,6 @@
 import type { NextConfig } from "next";
-import { fileURLToPath } from 'url';
-import path from 'path';
-
-const filename = fileURLToPath(import.meta.url);
-const dirname = path.dirname(filename);
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: dirname,
-  },
   output: 'standalone',
 };
 
